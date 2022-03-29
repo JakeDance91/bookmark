ae05e65fea0f232235c0329e816e6104d11f0543
Jake Dance <jake.dance1991@gmail.com>
2022-03-29 13:17:19 +0100

    added /bookmarks/new path to add bookmarks

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -8,12 +8,19 @@ describe Bookmark do
       connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
       connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
       connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
-      
+
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include("http://www.makersacademy.com")
       expect(bookmarks).to include("http://www.destroyallsoftware.com")
       expect(bookmarks).to include("http://www.google.com")
+    end
+  end
+  describe '.create' do
+    it 'creates a new bookmark' do
+      Bookmark.create(url: 'http://www.example.org')
+  
+      expect(Bookmark.all).to include 'http://www.example.org'
     end
   end
 end
